84b57b975d9207c4692f33f2688fde3befc2654b
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-02-21 13:00:52 +0100

    Initial Models Project

---
+++ LabyrinthExplorer.Logic/Models/GameElements/CharacterElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Logic.Models.GameElements
+{
+    public abstract class CharacterElement : GameElement
+    {
+        public byte Health { get; set; }
+        public List<ItemElement> Inventory { get; set; }
+    }
+}
---
+++ LabyrinthExplorer.Logic/Models/GameElements/GameElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Logic.Models.GameElements
+{
+    public abstract class GameElement
+    {
+        public string Name { get; set; }
+        public Coordinates Position { get; set; }
+        public bool Visible { get; set; }
+        public char Model { get; set; }
+        public char AlternateModel { get; set; }
+        public bool MoveThrough { get; set; }
+    }
+}
