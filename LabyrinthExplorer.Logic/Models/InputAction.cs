58647fbabee81a6750f2a39879a2e2498ecad1d3
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-02 10:21:13 +0100

    Added InputAction

---
+++ LabyrinthExplorer.Logic/Models/InputAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Logic.Models
+{
+    public enum InputAction
+    {
+        Up,
+        Right,
+        Down,
+        Left,
+        UseWeapon,
+        UseDoors,
+        ExitToMenu,
+        Unknown
+    }
+}
