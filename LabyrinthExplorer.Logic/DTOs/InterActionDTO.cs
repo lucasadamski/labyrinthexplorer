4d19c95dc64331e1231273f43147ffb2d8ef1446
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-02 16:08:42 +0100

    Implemented InterActionDTO

---
+++ LabyrinthExplorer.Logic/DTOs/InterActionDTO.cs
@@ -0,0 +1,20 @@
+﻿using LabyrinthExplorer.Logic.Models;
+using LabyrinthExplorer.Logic.Models.GameElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Logic.DTOs
+{
+    //Used as a internal communication protocol inside GameEngine between
+    //GameEngine and HumanPlayer -> it sends a piece of GameMap to Player
+    //along with input, so Player independly decides what to do with it
+    public class InterActionDTO 
+    {
+        public GameElement[][] MapOfElements { get; set; }
+        public DTO DTO { get; set; }
+        public InputAction InputAction{ get; set; }
+    }
+}
