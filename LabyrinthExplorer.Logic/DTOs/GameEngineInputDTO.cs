4a0e7e20afe66fe418d6f62722b99e31fff28eac
Łukasz Adamczyk <lukaszadamczy@gmail.com>
2024-03-02 10:26:02 +0100

    Added GameEngine UI Communication protocol DTO

---
+++ LabyrinthExplorer.Logic/DTOs/GameEngineInputDTO.cs
@@ -0,0 +1,16 @@
+﻿using LabyrinthExplorer.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Logic.DTOs
+{
+    public class GameEngineInputDTO
+    {
+        public InputAction InputAction { get; set; } = InputAction.Unknown;
+        public DTO DTO { get; set; } = new DTO();
+
+    }
+}
